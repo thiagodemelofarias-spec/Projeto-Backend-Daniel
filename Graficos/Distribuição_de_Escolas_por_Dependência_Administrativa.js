154ea09696d83d7d7b4d7de225dd8dd260995220
thiagodemelofarias-spec <thiagodemelo.farias@gmail.com>
2026-05-26 12:33:01 -0300

    Update Distribuição_de_Escolas_por_Dependência_Administrativa.js

--- Graficos/Distribuição_de_Escolas_por_Dependência_Administrativa.js
+++ Graficos/Distribuição_de_Escolas_por_Dependência_Administrativa.js
@@ -1,32 +1,35 @@
 var grafico04 = {
-  {
-  const linhas = vl.markLine({ strokeWidth: 2 })
-    .encode(
-      vl.color().fieldN("tipo").title("Tipo de Rede")
-    )
+  
+  especificacao: (() => {
+    const linhas = vl.markLine({ strokeWidth: 2 })
+      .encode(
+        vl.color().fieldN("tipo").title("Tipo de Rede")
+      );
 
-  const pontos = vl.markPoint({ filled: true, size: 60 })
-    .encode(
-      vl.color().fieldN("tipo").title("Tipo de Rede")
-    )
+    const pontos = vl.markPoint({ filled: true, size: 60 })
+      .encode(
+        vl.color().fieldN("tipo").title("Tipo de Rede")
+      );
 
-  return vl.layer(linhas, pontos)
-    .data(dadosLinha)
-    .encode(
-      vl.x().fieldN("regiao")
-            .title("Localidade da Escola")
-            .sort(["Norte", "Nordeste", "Centro-Oeste", "Sudeste", "Sul"]),
-      vl.y().fieldQ("escolas")
-            .title("Quantidade Total de Escolas"),
-    )
-    .title({
-      text: "Distribuição do Número de Escolas por Dependência Administrativa",
-      fontSize: 14,
-      fontWeight: "bold"
-    })
-    .width(600)
-    .height(350)
-    .render()
-}
+   
+    return vl.layer(linhas, pontos)
+      .data(dadosLinha)
+      .encode(
+        vl.x().fieldN("regiao")
+              .title("Localidade da Escola")
+              .sort(["Norte", "Nordeste", "Centro-Oeste", "Sudeste", "Sul"]),
+        vl.y().fieldQ("escolas")
+              .title("Quantidade Total de Escolas") 
+      )
+      .title({
+        text: "Distribuição do Número de Escolas por Dependência Administrativa",
+        fontSize: 14,
+        fontWeight: "bold"
+      })
+      .width(600)
+      .height(350)
+      .render();
+  })() 
 };
-vegaEmbed('#Distribuição_de_Escolas_por_Dependência_Administrativa', grafico04);
+
+vegaEmbed('#Distribuição_de_Escolas_por_Dependência_Administrativa', grafico04.especificacao);
