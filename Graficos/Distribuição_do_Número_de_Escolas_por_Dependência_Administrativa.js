03fb370f85a0be7802a83ab15c7a1b8a79860ed4
thiagodemelofarias-spec <thiagodemelo.farias@gmail.com>
2026-05-26 12:31:21 -0300

    Update Distribuição_do_Número_de_Escolas_por_Dependência_Administrativa.js

--- Graficos/Distribuição_do_Número_de_Escolas_por_Dependência_Administrativa.js
+++ Graficos/Distribuição_do_Número_de_Escolas_por_Dependência_Administrativa.js
@@ -1,15 +1,18 @@
 var grafico01 = {
-  vl.markBar({ color: "skyblue", stroke: "black", strokeWidth: 1 })
-  .data(dadosGrafico)
-  .encode(
-    vl.x().fieldQ("escolas").title("Tipo de Rede (Categoria 2)"),
-    vl.y().fieldN("categoria")
-          .title("Quantidade Total de Escolas")
-          .sort(["Privada", "Municipal", "Federal", "Estadual"]),
-  )
-  .title("Distribuição do Número de Escolas por Dependência Administrativa")
-  .width(700)
-  .height(300)
-  .render()
+  
+  especificacao: vl.markBar({ color: "skyblue", stroke: "black", strokeWidth: 1 })
+    .data(dadosGrafico)
+    .encode(
+      vl.x().fieldQ("escolas").title("Tipo de Rede (Categoria 2)"),
+      vl.y().fieldN("categoria")
+            .title("Quantidade Total de Escolas")
+            .sort(["Privada", "Municipal", "Federal", "Estadual"]) // Removida a vírgula sobressalente aqui
+    )
+    .title("Distribuição do Número de Escolas por Dependência Administrativa")
+    .width(700)
+    .height(300)
+    .render()
 };
-vegaEmbed('#Distribuição_do_Número_de_Escolas_por_Dependência_Administrativa', grafico01);
+
+
+vegaEmbed('#Distribuição_do_Número_de_Escolas_por_Dependência_Administrativa', grafico01.especificacao);
