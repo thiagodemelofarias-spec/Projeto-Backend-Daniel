cac31a9b3a7a2fe6d17993f7371174125e901ea4
thiagodemelofarias-spec <thiagodemelo.farias@gmail.com>
2026-05-28 18:18:29 -0300

    Update Distribuição_de_Escolas_por_Região_e_Dependência_Administrativa.js

--- Graficos/Distribuição_de_Escolas_por_Região_e_Dependência_Administrativa.js
+++ Graficos/Distribuição_de_Escolas_por_Região_e_Dependência_Administrativa.js
@@ -1,29 +1,52 @@
-var grafico03 = {
-  
-  especificacao: vl.markBar()
-    .data(dadosRegioes)
-    .encode(
-      vl.x().fieldN("regiao")
-            .title("Região")
-            .sort(["Norte", "Nordeste", "Centro-Oeste", "Sudeste", "Sul"]),
-      vl.y().fieldQ("escolas")
-            .aggregate("sum")
-            .title("Quantidade de Escolas")
-            .stack(true),
-      vl.color().fieldN("tipo")
-                .title("Tipo de Gestão")
-                .scale({ scheme: "viridis" }),  
-      vl.order().fieldN("tipo")                 
-    )
-    .title({
-      text: "Distribuição de Escolas por Região e Dependência Administrativa",
-      fontSize: 14,
-      fontWeight: "bold"
-    })
-    .width(600)
-    .height(400)
-    .render() 
+const especGrafico03 = {
+  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+  "title": {
+    "text": "Distribuição de Escolas por Região e Dependência Administrativa",
+    "fontSize": 14,
+    "fontWeight": "bold"
+  },
+  "width": 600,
+  "height": 400,
+  "data": { 
+    "url": "dados/meus_dados_processados.csv",
+    "format": {
+      "type": "csv",
+      "parse": {
+        "Escolas": "number" 
+      }
+    }
+  },
+  "transform": [
+    { "filter": "datum['Localidade da Escola'] !== 'Brasil'" }
+  ],
+  "mark": { "type": "bar", "tooltip": true },
+  "encoding": {
+    "x": {
+      "field": "Localidade da Escola",
+      "type": "nominal",
+      "title": "Região",
+      "sort": ["Norte", "Nordeste", "Centro-Oeste", "Sudeste", "Sul"],
+      "axis": { "labelAngle": 0 }
+    },
+    "y": {
+      "aggregate": "sum", 
+      "field": "Escolas", 
+      "type": "quantitative",
+      "title": "Quantidade de Escolas"
+    },
+    "color": {
+      "field": "Categoria 2",
+      "type": "nominal",
+      "title": "Tipo de Gestão",
+      "scale": { "scheme": "viridis" }
+    },
+    "order": { 
+      "field": "Categoria 2", 
+      "type": "nominal", 
+      "sort": "ascending"
+    }
+  }
 };
 
 
-vegaEmbed('#Distribuição_de_Escolas_por_Região_e_Dependência_Administrativa', grafico03.especificacao);
+vegaEmbed('#Distribuição_de_Escolas_por_Região_e_Dependência_Administrativa', especGrafico03);
